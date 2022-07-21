06105d26ba0f41100d21aafee0eb5324e11a6d11
Fernanda Vidal <fevidal.dev@gmail.com>
2022-07-21 11:04:00 -0300

    Req 45 em processo.

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,35 +1,145 @@
 import React from 'react';
-// import Footer from '../components/Footer';
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
+import '../css/doneRecipes.css';
+// import DoneRecipesFood from '../components/DoneRecipesFood';
 
 function DoneRecipes() {
+  const teste = [
+    {
+      id: '52771',
+      type: 'comida',
+      nationality: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Spicy Arrabiata Penne',
+      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+      doneDate: '23/06/2020',
+      tags: ['Pasta', 'Curry'],
+    },
+    {
+      id: '178319',
+      type: 'bebida',
+      nationality: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Aquamarine',
+      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+      doneDate: '23/06/2020',
+      tags: [],
+    },
+  ];
+
+  localStorage.setItem('doneRecipes', JSON.stringify(teste));
+
+  const handleClickShare = (id) => {
+    const url = `http://localhost:3000/foods/${id}`;
+    console.log(url);
+    navigator.clipboard.writeText(url);
+  };
+
   const callLocalStorage = () => {
     const getLocalStorage = JSON.parse(localStorage.getItem('doneRecipes'));
 
-    console.log(getLocalStorage);
-    /* if (getLocalStorage !== null) { */
-    const retunStorage = getLocalStorage
-      .map(({ image, name, category, doneDate, tags }, index) => (
-        <div key={ index + 1 }>
-          <img src={ image } alt="name" data-testid={ `${index}-horizontal-image` } />
-          <p data-testid={ `${index}-horizontal-top-text` }>{ category }</p>
-          <h3 data-testid={ `${index}-horizontal-name` }>{ name }</h3>
-          <button type="button" data-testid={ `${index}-horizontal-share-btn` }>
-            <img src={ shareIcon } alt="shareIcon" />
-          </button>
-          <p data-testid={ `${index}-horizontal-done-date` }>{ doneDate }</p>
-          { tags.map((tag, i) => (
-            <span
-              testid={ `${i}-${tag}-horizontal-tag` }
-              key={ i + 1 }
-            >
-              { tag }
-              {' '}
-            </span>))}
-        </div>
-      ));
-    return retunStorage;
+    if (getLocalStorage !== null) {
+      const retunStorage = getLocalStorage
+        .map(({
+          id,
+          image,
+          name,
+          category,
+          doneDate,
+          tags,
+          type,
+          nationality,
+          alcoholicOrNot,
+        }, index) => {
+          if (type === 'comida') {
+            return (
+              // <DoneRecipesFood image category name nationality doneDate tags index />
+              <div className="container-dones" key={ index + 1 }>
+                <div className="container-dones-img">
+                  <img
+                    className="img-recipes-done"
+                    src={ image }
+                    alt="name"
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                </div>
+                <div className="container-dones-info">
+                  <div className="container-dones-info-share">
+                    <p data-testid={ `${index}-horizontal-top-text` }>{ category }</p>
+                    <p>-</p>
+                    <p
+                      data-testid={ `${index}-horizontal-nationality` }
+                    >
+                      { nationality }
+                    </p>
+                    <button
+                      type="button"
+                      data-testid={ `${index}-horizontal-share-btn` }
+                      onClick={ () => handleClickShare(id) }
+                    >
+                      <img className="shareIcon" src={ shareIcon } alt="shareIcon" />
+                    </button>
+                  </div>
+                  <h3 data-testid={ `${index}-horizontal-name` }>{ name }</h3>
+                  <p
+                    data-testid={ `${index}-horizontal-done-date` }
+                  >
+                    { `Don in: ${doneDate}` }
+                  </p>
+                  <div className="container-dones-info-name">
+                    { tags.map((tag, i) => (
+                      <span
+                        data-testid={ `${i}-${tag}-horizontal-tag` }
+                        key={ i + 1 }
+                      >
+                        { tag }
+                        {' '}
+                      </span>))}
+                  </div>
+                </div>
+              </div>
+            );
+          }
+          return (
+            <div className="container-dones" key={ index + 1 }>
+              <div className="container-dones-img">
+                <img
+                  className="img-recipes-done"
+                  src={ image }
+                  alt="name"
+                  data-testid={ `${index}-horizontal-image` }
+                />
+              </div>
+              <div className="container-dones-info">
+                <div className="container-dones-info-share">
+                  <p
+                    data-testid={ `${index}-horizontal-alcoholicOrNot` }
+                  >
+                    { alcoholicOrNot }
+                  </p>
+                  <button
+                    type="button"
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    onClick={ handleClickShare }
+                  >
+                    <img src={ shareIcon } alt="shareIcon" />
+                  </button>
+                </div>
+                <h3 data-testid={ `${index}-horizontal-name` }>{ name }</h3>
+                <p
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  { `Done in: ${doneDate}` }
+                </p>
+              </div>
+            </div>
+          );
+        });
+      return retunStorage;
+    }
   };
 
   return (
