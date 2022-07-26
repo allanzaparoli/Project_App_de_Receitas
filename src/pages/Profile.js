e2a284d954d9b3e2e324e67c05c7e26a4c6861ae
Fernanda Vidal <fevidal.dev@gmail.com>
2022-07-26 10:32:53 -0300

    CSS profile.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../css/profile.css';
 
 function Profile() {
   const emailSaved = JSON.parse(localStorage.getItem('user'))?.email;
@@ -24,28 +25,33 @@ function Profile() {
   return (
     <div>
       <Header title="Profile" profile />
-      <p data-testid="profile-email">{ emailSaved }</p>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ doneRecipesClick }
-      >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ FavoriteRecipesClick }
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ logoutClick }
-      >
-        Logout
-      </button>
+      <p className="email-profile" data-testid="profile-email">{ emailSaved }</p>
+      <div className="container-btn-profile">
+        <button
+          className="btn-profile"
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ doneRecipesClick }
+        >
+          Done Recipes
+        </button>
+        <button
+          className="btn-profile"
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ FavoriteRecipesClick }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          className="btn-profile"
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ logoutClick }
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </div>
   );
